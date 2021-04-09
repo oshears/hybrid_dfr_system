4a44291d7d502090ec24e65fb76500aeac3e418b
Osaze Shears <oshears@vt.edu>
2021-04-09 13:23:55 -0400

    finalized linux example

--- linux/code_examples/asic_function_demo.py
+++ linux/code_examples/asic_function_demo.py
@@ -11,18 +11,26 @@ regs = asic_function_regs
 CTRL_REG_ADDR = 0x0
 ASIC_OUT_REG_ADDR = 0x4
 ASIC_IN_REG_ADDR = 0x8
+ASIC_DONE = 0x2
 
 for i in range(16):
-    dac_data = i * 0x1000
-    
-    print(f"Writing: {hex(dac_data)}")
 
-    regs[ASIC_OUT_REG_ADDR : ASIC_OUT_REG_ADDR + 4] = bytes([dac_data & 0xF, (dac_data >> 8) & 0xF, 0x00, 0x00])
+    dac_data = i * 0x1111
+    encoded_dac_data = bytes([dac_data & 0x0F, (dac_data >> 8) & 0x0F, 0x00, 0x00])
+
+    print(f"Writing: {hex(dac_data)}")
+    print(f"Writing: {encoded_dac_data}")
+
+    regs[ASIC_OUT_REG_ADDR : ASIC_OUT_REG_ADDR + 4] = encoded_dac_data
     regs[CTRL_REG_ADDR] = 0x1
 
     time.sleep(0.25)
+    while(regs[CTRL_REG_ADDR] != ASIC_DONE):
+        continue
 
-    print(regs[CTRL_REG_ADDR])
     results_bytes = regs[ASIC_IN_REG_ADDR : ASIC_IN_REG_ADDR + 4]
     results = int.from_bytes(results_bytes,"little")
-    print(f"Result: {results}")
+    print(f"Result (Bytes): {results_bytes}")
+    print(f"Result: {hex(results)}")
+
+    print("============================")
