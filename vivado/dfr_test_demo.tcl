116bd5d2b33c2b3a5ec70536af77792522b7855e
Osaze Shears <oshears@vt.edu>
2021-05-12 12:51:02 -0400

    updated dfr jtag axi test

--- vivado/dfr_test_demo.tcl
+++ vivado/dfr_test_demo.tcl
@@ -1,63 +1,24 @@
-reset_hw_axi [get_hw_axis]
-create_hw_axi_txn txn_1 [get_hw_axis hw_axi_1] -address 00000000 -data {DEADBEE0} -len 1 -size 32 -type write
-create_hw_axi_txn txn_2 [get_hw_axis hw_axi_1] -address 00000000 -len 1 -size 32 -type read
-run_hw_axi txn_1 txn_2 -queue
-#poke_hw_axi [get_hw_axi_txns txn1]
-#peek_hw_axi [get_hw_axi_txns txn1]
-
-
-# AXI LITE
-reset_hw_axi [get_hw_axis]
-create_hw_axi_txn txn_0 [get_hw_axis hw_axi_1] -address 00000000 -data 00000000 -type write
-create_hw_axi_txn txn_1 [get_hw_axis hw_axi_1] -address 00000000 -data DEADBEE0 -type write
-create_hw_axi_txn txn_2 [get_hw_axis hw_axi_1] -address 00000000 -type read
-create_hw_axi_txn txn_3 [get_hw_axis hw_axi_1] -address 40000000 -data DEADBEEF -type write
-create_hw_axi_txn txn_4 [get_hw_axis hw_axi_1] -address 40000000 -type read
-create_hw_axi_txn txn_5 [get_hw_axis hw_axi_1] -address 01000000 -data DEADBEEF -type write
-create_hw_axi_txn txn_6 [get_hw_axis hw_axi_1] -address 01000000 -type read
-# create_hw_axi_txn txn_3 [get_hw_axis hw_axi_1] -address 00000004 -data ABCD0123 -type write
-# create_hw_axi_txn txn_4 [get_hw_axis hw_axi_1] -address 00000004 -type read
-run_hw_axi txn_1 
-run_hw_axi txn_2
-#poke_hw_axi [get_hw_axi_txns txn1]
-#peek_hw_axi [get_hw_axi_txns txn1]
-
-reset_hw_axi [get_hw_axis]
-create_hw_axi_txn ctrl_reg_clr  [get_hw_axis hw_axi_1]  -address 0x00000000 -data 00000000 -type write -force
-create_hw_axi_txn ctrl_reg_test [get_hw_axis hw_axi_1]  -address 0x00000000 -data DEADBEE0 -type write -force
-create_hw_axi_txn ctrl_reg_read [get_hw_axis hw_axi_1]  -address 0x00000000 -type read -force
-run_hw_axi ctrl_reg_clr
-run_hw_axi ctrl_reg_read
-run_hw_axi ctrl_reg_test
-run_hw_axi ctrl_reg_read
-
-reset_hw_axi [get_hw_axis]
-create_hw_axi_txn ctrl_reg_clr [get_hw_axis hw_axi_1]  -address 0x40000000 -data 12340000 -type write -force
-create_hw_axi_txn ctrl_reg_test [get_hw_axis hw_axi_1] -address 0x40000000 -data DEADBEE0 -type write -force
-create_hw_axi_txn ctrl_reg_read [get_hw_axis hw_axi_1] -address 0x40000000 -type read -force
-run_hw_axi ctrl_reg_clr
-run_hw_axi ctrl_reg_read
-run_hw_axi ctrl_reg_test
-run_hw_axi ctrl_reg_read
-
-
-
 # DFR Task
-create_hw_axi_txn input_mem_write_0  [get_hw_axis hw_axi_1]  -address 01000000 -data DEADBEEF -type write -force
-create_hw_axi_txn input_mem_read_0   [get_hw_axis hw_axi_1]  -address 01000000 -type read -force
-create_hw_axi_txn reservoir_mem_write_0  [get_hw_axis hw_axi_1]  -address 02000000 -data DEADBEEF -type write -force
-create_hw_axi_txn reservoir_mem_read_0   [get_hw_axis hw_axi_1]  -address 02000000 -type read -force
-create_hw_axi_txn weight_mem_write_0  [get_hw_axis hw_axi_1]  -address 03000000 -data DEADBEEF -type write -force
-create_hw_axi_txn weight_mem_read_0   [get_hw_axis hw_axi_1]  -address 03000000 -type read -force
-create_hw_axi_txn dfr_mem_write_0  [get_hw_axis hw_axi_1]  -address 04000000 -data DEADBEEF -type write -force
-create_hw_axi_txn dfr_mem_read_0   [get_hw_axis hw_axi_1]  -address 04000000 -type read -force
+create_hw_axi_txn num_init_sample_reg_write  [get_hw_axis hw_axi_1]  -address 00000008 -data 00000000 -type write -force
+create_hw_axi_txn num_train_sample_reg_write  [get_hw_axis hw_axi_1]  -address 0000000C -data 00000000 -type write -force
+create_hw_axi_txn num_test_sample_reg_write  [get_hw_axis hw_axi_1]  -address 00000010 -data 00000001 -type write -force
+create_hw_axi_txn num_samples_per_reg_write  [get_hw_axis hw_axi_1]  -address 00000014 -data 00000064 -type write -force
+create_hw_axi_txn num_init_steps_reg_write  [get_hw_axis hw_axi_1]  -address 00000018 -data 00000000 -type write -force
+create_hw_axi_txn num_train_steps_reg_write  [get_hw_axis hw_axi_1]  -address 0000001C -data 00000000 -type write -force
+create_hw_axi_txn num_test_steps_reg_write  [get_hw_axis hw_axi_1]  -address 00000020 -data 00000064 -type write -force
+create_hw_axi_txn input_mem_write_0  [get_hw_axis hw_axi_1]  -address 01000000 -data 00000001 -type write -force
+create_hw_axi_txn ctrl_reg_write  [get_hw_axis hw_axi_1]  -address 00000000 -data 00000001 -type write -force
+create_hw_axi_txn reservoir_out_mem_read_0   [get_hw_axis hw_axi_1]  -address 02000000 -type read -force
 
 reset_hw_axi [get_hw_axis]
+run_hw_axi num_init_sample_reg_write
+run_hw_axi num_train_sample_reg_write
+run_hw_axi num_test_sample_reg_write
+run_hw_axi num_samples_per_reg_write
+run_hw_axi num_init_steps_reg_write
+run_hw_axi num_train_steps_reg_write
+run_hw_axi num_test_steps_reg_write
 run_hw_axi input_mem_write_0
-run_hw_axi input_mem_read_0
-run_hw_axi reservoir_mem_write_0
-run_hw_axi reservoir_mem_read_0
-run_hw_axi weight_mem_write_0
-run_hw_axi weight_mem_read_0
-run_hw_axi dfr_mem_write_0
-run_hw_axi dfr_mem_read_0
+run_hw_axi ctrl_reg_write
+run_hw_axi reservoir_out_mem_read_0
+
