6ecdc6a8eb3b8128a5751d962a19aaa4d00d8826
Osaze Shears <oshears@vt.edu>
2021-05-13 19:06:52 -0400

    added int spectrum sensing model

--- python/dfr_sw_int_spectrum.py
+++ python/dfr_sw_int_spectrum.py
@@ -1,7 +1,7 @@
 ############################################################
 #
 #   Delay-feedback Reservoir (DFR)
-#   Spectrum (Float)
+#   Spectrum (Int)
 #
 ############################################################
 
@@ -9,7 +9,11 @@ import numpy as np
 
 MG_FUNCTION_RESOLUTION = 2**16
 
-MAX_INPUT = 0
+MAX_INPUT = 2**16
+
+MAX_MG_OUTPUT = 2**12
+
+YT_SCALE = 2**32
 
 
 # ASIC Mackey-Glass Activation Function
@@ -17,12 +21,16 @@ mg_vector = np.genfromtxt(f"./data/asic_function_onboard_dac.csv", delimiter=","
 
 def mackey_glass(inData):
 
-    if inData < MG_FUNCTION_RESOLUTION:
-        # scaled_input = int(MG_FUNCTION_RESOLUTION * inData)
-        scaled_input = int(MG_FUNCTION_RESOLUTION * inData / MAX_INPUT )
-        if scaled_input < MG_FUNCTION_RESOLUTION and scaled_input > 0:
-            float_output = mg_vector[1,scaled_input] / (2**12)
-            return float_output
+    # scaled the input based on the max MG function input (MG_FUNCTION_RESOLUTION)
+    # and the max input data value
+    scaled_input = int( MG_FUNCTION_RESOLUTION * ( inData / MAX_INPUT ) )
+
+    # if the scaled result is in the range of the MG function
+    if scaled_input < MG_FUNCTION_RESOLUTION and scaled_input >= 0:
+        
+        # scale the output back within the range of the max input values
+        int_output = MAX_INPUT * ( mg_vector[1,scaled_input] / MAX_MG_OUTPUT )
+        return int_output
     
     return 0
 
@@ -46,6 +54,7 @@ target = spectrum_vector[:,1].reshape((1,spectrum_vector.shape[0]))
 # initLen	= number of samples used in initialization
 # trainLen	= number of samples used in training
 # testLen	= number of samples used in testing
+
 NUM_SAMPLES = 6102
 Tp          = 100
 N           = Tp
@@ -59,10 +68,8 @@ testLen     = NUM_SAMPLES - (trainLen + initLen + initLen)
 
 ##  Define the masking (input weight, choose one of the followings)
 
-# Random Uniform [0, 1]
-M = np.random.rand(Tp, 1)
-# M = np.random.rand(Tp,1) * 2 - 1
-# M = np.sign(M) * 0.1
+# Random Uniform [0, MAX_INPUT]
+M = np.random.rand(Tp, 1) * MAX_INPUT
 
 ##  (Training) Initialization of reservoir dynamics
 
@@ -72,31 +79,29 @@ M = np.random.rand(Tp, 1)
 # nodeTR    = a snapshot of all node states at each full rotation through 
 #             the reservoir during training
 
-nodeC   = np.zeros(shape=(N, 1))
-nodeN   = np.zeros(shape=(N, 1))
-nodeE	= np.zeros(shape=(N , trainLen * Tp))
-nodeTR	= np.zeros(shape=(N , trainLen))
+nodeC   = np.zeros(shape=(N, 1),dtype=int)
+nodeN   = np.zeros(shape=(N, 1),dtype=int)
+nodeE	= np.zeros(shape=(N , trainLen * Tp),dtype=int)
+nodeTR	= np.zeros(shape=(N , trainLen),dtype=int)
 
 
 ##  (Training) Apply masking to training data
-inputTR = np.ndarray(shape=((initLen + trainLen) * Tp,1))
+inputTR = np.ndarray(shape=((initLen + trainLen) * Tp,1),dtype=int)
 
 for k in range(0,(initLen + trainLen)):
     uTR = data[0,k]
+    # multiply input by mask and convert to int
     masked_input = (M * uTR)
     inputTR[k*Tp:(k+1)*Tp] = masked_input.copy()
 
-MAX_INPUT = np.max(inputTR)
-
 ##  (Training) Initialize the reservoir layer
-
 # No need to store these values since they won't be used in training
 for k in range(0,(initLen * Tp)):
-
     # Compute the new input data for initialization
     initJTR = (inputTR[k,0]) + eta * (nodeC[N-1,0])
     
     # Activation
+    # multiply by 8 to scale 12-bit output to 16 bits (15 bits unsigned)
     nodeN[0,0]	= (mackey_glass(initJTR))
     nodeN[1:N]  = nodeC[0:(N - 1)]
     
@@ -106,7 +111,6 @@ for k in range(0,(initLen * Tp)):
 
 ##	(Training) Run data through the reservoir
 for k in range(0,(trainLen * Tp)):
-
     # Define the time step that starts storing node states
     t = initLen * Tp + k
     
@@ -131,18 +135,20 @@ nodeTR[:,0:trainLen] = nodeE[:, N*np.arange(1,trainLen + 1)-1]
 ##  Train output weights using ridge regression
 
 # Call-out the target outputs
-Yt = target[0,initLen:(initLen + trainLen)].reshape(1,trainLen)
+# Scale to put the data in the same range as input
+Yt = target[0,initLen:(initLen + trainLen)].reshape(1,trainLen) * YT_SCALE
 
 # Transpose nodeR for matrix claculation
 nodeTR_T = nodeTR.T
 
 # Calculate output weights
-reg = 1e-8
-Wout = np.dot(np.dot(Yt,nodeTR_T),np.linalg.inv((np.dot(nodeTR,nodeTR_T)) + reg * np.eye(N)))
+# initialize regularization coefficient
+reg = 1e8
+Wout = np.round( np.dot(np.dot(Yt,nodeTR_T),np.linalg.inv((np.dot(nodeTR,nodeTR_T)) + reg * np.eye(N))) )
 
 ##  Compute training error
 predicted_target = np.dot(Wout,nodeTR)
-predicted_target = predicted_target > 0.5
+# predicted_target = predicted_target > np.max(predicted_target) / 2
 
 # Calculate the MSE through L2 norm
 mseTR = (((Yt - predicted_target)**2).mean(axis=1))
@@ -168,14 +174,14 @@ print(f'training NMSE    = {nmseTR}')
 # nodeTS    = a snapshot of all node states at each full rotation through 
 #             the reservoir during testing
 
-nodeC   = np.zeros(shape=(N, 1))
-nodeN   = np.zeros(shape=(N, 1))
-nodeE	= np.zeros(shape=(N , testLen * Tp))
-nodeTS	= np.zeros(shape=(N , testLen))
+nodeC   = np.zeros(shape=(N, 1),dtype=int)
+nodeN   = np.zeros(shape=(N, 1),dtype=int)
+nodeE	= np.zeros(shape=(N , testLen * Tp),dtype=int)
+nodeTS	= np.zeros(shape=(N , testLen),dtype=int)
 
 
 ##  (Testing) Apply masking to input testing data
-inputTS = np.ndarray(shape=((initLen + testLen) * Tp,1))
+inputTS = np.ndarray(shape=((initLen + testLen) * Tp,1),dtype=int)
 
 for k in range(0,(initLen + testLen)):
     uTS = data[0,initLen + trainLen + k]
@@ -202,7 +208,6 @@ for k in range(0,(initLen * Tp)):
 
 ##  (Testing) Run data through the reservoir
 for k in range(0,(testLen * Tp)):
-
     # Define the time step that starts storing node states
     t = initLen * Tp + k
     
@@ -224,14 +229,13 @@ for k in range(0,(testLen * Tp)):
 # Consider the data just once everytime it loops around
 nodeTS[:,0:testLen] = nodeE[:, N*np.arange(1,testLen + 1)-1]
 
-
 ##  Compute testing errors
 
 # Call-out the target outputs
-Yt = target[0,initLen + trainLen + initLen : initLen + trainLen + initLen + testLen].reshape(1,testLen)
+Yt = target[0,initLen + trainLen + initLen : initLen + trainLen + initLen + testLen].reshape(1,testLen) * YT_SCALE
 
 predicted_target = np.dot(Wout,nodeTS)
-predicted_target = predicted_target > 0.5
+# predicted_target = predicted_target > np.max(predicted_target) / 2
 
 # Calculate the MSE through L2 norm
 mse_testing = (((Yt - predicted_target)**2).mean(axis=1))
@@ -248,6 +252,40 @@ print(f'testing MSE     = {mse_testing[0]}')
 print(f'testing NMSE    = {nmse_testing}')
 # print(f'testing NRMSE    = {nrmse_testing}')
 
+########################################################################
+########################################################################
+
+### Write Data for Hybrid System Simulation ###
+# Save Inputs
+fh = open("./data/spectrum/dfr_sw_int_spectrum_inputs.txt","w")
+for i in range(inputTS.size):
+    fh.write(f"{inputTS[i,0]}\n")
+fh.close()
+
+# Save Reservoir Outputs
+fh = open("./data/spectrum/dfr_sw_int_spectrum_reservoir_outputs.txt","w")
+for input_idx in range(nodeTS.shape[1]):
+    for sample_idx in range(nodeTS.shape[0] - 1, 0,-1):
+        fh.write(f"{nodeTS[sample_idx,input_idx]}\n")
+fh.close()
+
+# Save Weights
+fh = open("./data/spectrum/dfr_sw_int_spectrum_weights.txt","w")
+for i in range(Wout.size):
+    fh.write(f"{int(Wout[0,i])}\n")
+fh.close()
+
+# Save DFR Outputs
+fh = open("./data/spectrum/dfr_sw_int_spectrum_dfr_outputs.txt","w")
+for i in range(predicted_target.size):
+    fh.write(f"{int(predicted_target[0,i])}\n")
+fh.close()
+
+# Save Expected Spectrum Sensing Outputs
+fh = open("./data/spectrum/dfr_sw_int_spectrum_expected_dfr_outputs.txt","w")
+for i in range(Yt.size):
+    fh.write(f"{int(Yt[0,i])}\n")
+fh.close()
 
 # DFR: An Energy-efficient Analog Delay Feedback Reservoir Computing System
 # Table 2. Performance Comparison in Different Models
@@ -264,4 +302,4 @@ x = np.linspace(0,SAMPLES-1,SAMPLES)
 plt.plot(x,Yt[0,0:SAMPLES],label="Yt")
 plt.plot(x,predicted_target[0,0:SAMPLES],'--',label="Predicted Target")
 plt.legend()
-plt.savefig("./data/dfr_sw_float_spectrum_fig.png")
+plt.savefig("./data/dfr_sw_int_spectrum_fig.png")
