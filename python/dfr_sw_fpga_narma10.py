57928f3a72b339066d42137816bc4663f21ec02f
Osaze Shears <oshears@vt.edu>
2021-05-13 14:23:46 -0400

    updated output files and fpga dfr

--- python/dfr_sw_fpga_narma10.py
+++ python/dfr_sw_fpga_narma10.py
@@ -52,28 +52,6 @@ def mackey_glass(inData):
     
     return 0
 
-# NARMA10
-def narma10_create(inLen):
-
-    # Compute the random uniform input matrix
-    inp = 0.5*np.random.rand(1, inLen)
-
-    # Compute the target matrix
-    tar = np.zeros(shape=(1, inLen))
-
-    for k in range(10,(inLen - 1)):
-        tar[0,k+1] = 0.3 * tar[0,k] + 0.05 * tar[0,k] * np.sum(tar[0,k-9:k]) + 1.5 * inp[0,k] * inp[0,k - 9] + 0.1
-    
-    return (inp, tar)
-
-##	Import dataset
-
-# 10th order nonlinear auto-regressive moving average (NARMA10)
-seed = 0
-np.random.seed(seed)
-data, target = narma10_create(10000)
-
-
 ##	Reservoir Parameters
 
 # Tp        = sample/hold time frame for input (length of input mask)
@@ -95,105 +73,6 @@ initLen     = 1
 trainLen	= 5900
 testLen     = 4000
 
-##  Define the masking (input weight, choose one of the followings)
-
-# Random Uniform [0, 1]
-M = np.random.rand(Tp, 1)
-
-##  (Training) Initialization of reservoir dynamics
-
-# nodeC     = reservoir dynamic at the current cycle
-# nodeN     = reservoir dynamic at the next cycle
-# nodeE     = reservoir dynamic at every timestep during training
-# nodeTR    = a snapshot of all node states at each full rotation through 
-#             the reservoir during training
-
-nodeC   = np.zeros(shape=(N, 1),dtype=int)
-nodeN   = np.zeros(shape=(N, 1),dtype=int)
-nodeE	= np.zeros(shape=(N , trainLen * Tp),dtype=int)
-nodeTR	= np.zeros(shape=(N , trainLen),dtype=int)
-
-
-##  (Training) Apply masking to training data
-inputTR = np.ndarray(shape=((initLen + trainLen) * Tp,1),dtype=int)
-
-for k in range(0,(initLen + trainLen)):
-    uTR = data[0,k]
-    # multiply input by mask and convert to int
-    masked_input = (M * uTR) * 2**16
-    inputTR[k*Tp:(k+1)*Tp] = masked_input.copy()
-
-##  (Training) Initialize the reservoir layer
-# No need to store these values since they won't be used in training
-for k in range(0,(initLen * Tp)):
-    # Compute the new input data for initialization
-    initJTR = (inputTR[k,0]) + eta * (nodeC[N-1,0])
-    
-    # Activation
-    # multiply by 8 to scale 12-bit output to 16 bits (15 bits unsigned)
-    nodeN[0,0]	= (mackey_glass(initJTR))
-    nodeN[1:N]  = nodeC[0:(N - 1)]
-    
-    # Update the current node state
-    nodeC       = nodeN.copy()
-
-
-##	(Training) Run data through the reservoir
-for k in range(0,(trainLen * Tp)):
-    # Define the time step that starts storing node states
-    t = initLen * Tp + k
-    
-    # Compute the new input data for training
-    trainJ = (inputTR[t,0]) + eta * (nodeC[N-1,0])
-    
-    # Activation
-    nodeN[0,0]	= (mackey_glass(trainJ))
-    nodeN[1:N]  = nodeC[0:(N - 1)]
-    
-    # Update the current node state
-    nodeC       = nodeN.copy()
-    
-    # Updete all node states
-    nodeE[:, k] = nodeC[:,0]
-
-
-
-# Consider the data just once everytime it loops around
-nodeTR[:,0:trainLen] = nodeE[:, N*np.arange(1,trainLen + 1)-1]
-
-##  Train output weights using ridge regression
-
-# Call-out the target outputs
-# Scale to put the data in the same range as input
-Yt = target[0,initLen:(initLen + trainLen)].reshape(1,trainLen) * 2**32
-
-# Transpose nodeR for matrix claculation
-nodeTR_T = nodeTR.T
-
-# Calculate output weights
-reg = 1e-8
-Wout = np.dot(np.dot(Yt,nodeTR_T),np.linalg.inv((np.dot(nodeTR,nodeTR_T)) + reg * np.eye(N)))
-Wout = np.round(Wout)
-
-##  Compute training error
-predicted_target = np.dot(Wout,nodeTR)
-
-# Calculate the MSE through L2 norm
-mseTR = (((Yt - predicted_target)**2).mean(axis=1))
-
-# Calculate the NMSE
-nmseTR  = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))**2
-nrmseTR = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
-# nmseTR  =         np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size
-# nrmseTR = np.sqrt(np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size)
-
-print('--------------------------------------------------')
-print('Training Errors')
-print(f'training MSE     = {mseTR[0]}')
-print(f'training NMSE    = {nmseTR}')
-# print(f'training NRMSE    = {nrmseTR}')
-
-
 ## (Testing) Initialize the reservoir layer
 
 # nodeC     = reservoir dynamic at the current cycle
@@ -207,17 +86,27 @@ nodeN   = np.zeros(shape=(N, 1),dtype=int)
 nodeE	= np.zeros(shape=(N , testLen * Tp),dtype=int)
 nodeTS	= np.zeros(shape=(N , testLen),dtype=int)
 
-
-##  (Testing) Apply masking to input testing data
+# Load DFR Input Samples
 inputTS = np.ndarray(shape=((initLen + testLen) * Tp,1),dtype=int)
+input_cntr = 0
+fh = open("./data/narma10/dfr_sw_int_narma10_inputs.txt","r")
+file_lines = fh.readlines()
+for file_line in file_lines:
+    if input_cntr < initLen + testLen:
+        input_sample = int(file_line.strip())
+        inputTS[input_cntr,0] = input_sample
+    input_cntr += 1
 
-for k in range(0,(initLen + testLen)):
-    uTS = data[0,initLen + trainLen + k]
-    masked_input = (M * uTS) * 2**16
-    inputTS[k*Tp:(k+1)*Tp] = masked_input.copy()
+# Load Weight Data
+Wout = np.ndarray(shape=(1,N),dtype=int)
+weight_cntr = 0
+fh = open("./data/narma10/dfr_sw_int_narma10_weights.txt","r")
+for file_line in file_lines:
+    if weight_cntr < N:
+        input_weight = int(file_line.strip())
+        Wout[0,weight_cntr] = input_weight
+    weight_cntr += 1
 
-
-## (Testing) Initialize the reservoir layer
 
 # No need to store these values since they won't be used in testing
 for k in range(0,(initLen * Tp)):
@@ -242,6 +131,7 @@ for k in range(0,(initLen * Tp)):
 
 ##  (Testing) Run data through the reservoir
 for k in range(0,(testLen * Tp)):
+
     # Define the time step that starts storing node states
     t = initLen * Tp + k
     
@@ -257,7 +147,7 @@ for k in range(0,(testLen * Tp)):
         print(nodeC[N-1,0])
         print(testJ)
         print(nodeN[0,0])
-    
+
     # Update the current node state
     nodeC       = nodeN.copy()
     
@@ -269,11 +159,19 @@ for k in range(0,(testLen * Tp)):
 # Consider the data just once everytime it loops around
 nodeTS[:,0:testLen] = nodeE[:, N*np.arange(1,testLen + 1)-1]
 
-
 ##  Compute testing errors
 
 # Call-out the target outputs
-Yt = target[0,initLen + trainLen + initLen + 1 : initLen + trainLen + initLen + 1 + testLen].reshape(1,testLen) * 2**32
+# Yt = target[0,initLen + trainLen + initLen + 1 : initLen + trainLen + initLen + 1 + testLen].reshape(1,testLen) * 2**32
+Yt = np.ndarray(shape=(1,testLen))
+expected_output_cntr = 0
+fh = open("./data/narma10/dfr_sw_int_narma10_expected_dfr_outputs.txt","r")
+file_lines = fh.readlines()
+for file_line in file_lines:
+    if expected_output_cntr < testLen:
+        Yt[0,expected_output_cntr] = float(file_line.strip())
+    expected_output_cntr += 1
+    
 
 predicted_target = np.dot(Wout,nodeTS)
 
@@ -292,38 +190,6 @@ print(f'testing MSE     = {mse_testing[0]}')
 print(f'testing NMSE    = {nmse_testing}')
 # print(f'testing NRMSE    = {nrmse_testing}')
 
-
-### Write Data for Hybrid System Simulation ###
-# Save Inputs
-fh = open("./data/narma10/dfr_sw_int_narma10_inputs.txt","w")
-for i in range(inputTS.size):
-    fh.write(f"{inputTS[i,0]}\n")
-fh.close()
-
-# Save Reservoir Outputs
-fh = open("./data/narma10/dfr_sw_int_narma10_reservoir_outputs.txt","w")
-for input_idx in range(nodeTS.shape[1]):
-    for sample_idx in range(nodeTS.shape[0] - 1, 0,-1):
-        fh.write(f"{nodeTS[sample_idx,input_idx]}\n")
-fh.close()
-
-# Save Weights
-fh = open("./data/narma10/dfr_sw_int_narma10_weights.txt","w")
-for i in range(Wout.size):
-    fh.write(f"{int(Wout[0,i])}\n")
-fh.close()
-
-# Save DFR Outputs
-fh = open("./data/narma10/dfr_sw_int_narma10_dfr_outputs.txt","w")
-for i in range(predicted_target.size):
-    fh.write(f"{int(predicted_target[0,i])}\n")
-fh.close()
-
-# Save Expected NARMA10 Outputs
-fh = open("./data/narma10/dfr_sw_int_narma10_expected_dfr_outputs.txt","w")
-for i in range(Yt.size):
-    fh.write(f"{int(Yt[0,i])}\n")
-fh.close()
 
 # DFR: An Energy-efficient Analog Delay Feedback Reservoir Computing System
 # Table 2. Performance Comparison in Different Models
