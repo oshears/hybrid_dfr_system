bcfc00efec4c0b05cd0289a2f68d5fb2a0ed2770
Osaze Shears <oshears@vt.edu>
2021-05-14 16:01:44 -0400

    updated error calculations for spectrum sensing models

--- python/dfr_sw_float_spectrum.py
+++ python/dfr_sw_float_spectrum.py
@@ -140,24 +140,16 @@ nodeTR_T = nodeTR.T
 reg = 1e-8
 Wout = np.dot(np.dot(Yt,nodeTR_T),np.linalg.inv((np.dot(nodeTR,nodeTR_T)) + reg * np.eye(N)))
 
-##  Compute training error
+# Calculate the NMSE
 predicted_target = np.dot(Wout,nodeTR)
 predicted_target = predicted_target > 0.5
-
-# Calculate the MSE through L2 norm
-mseTR = (((Yt - predicted_target)**2).mean(axis=1))
-
-# Calculate the NMSE
-nmseTR  = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))**2
-nrmseTR = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
-# nmseTR  =         np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size
-# nrmseTR = np.sqrt(np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size)
+mse   = np.sum(np.power(Yt - predicted_target,2)) / Yt.size
+nrmse = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
 
 print('--------------------------------------------------')
-print('Training Errors')
-print(f'training MSE     = {mseTR[0]}')
-print(f'training NMSE    = {nmseTR}')
-# print(f'training NRMSE    = {nrmseTR}')
+print('Testing Errors')
+print(f'testing mse: {mse}')
+print(f'testing nrmse: {nrmse}')
 
 
 ## (Testing) Initialize the reservoir layer
@@ -230,23 +222,16 @@ nodeTS[:,0:testLen] = nodeE[:, N*np.arange(1,testLen + 1)-1]
 # Call-out the target outputs
 Yt = target[0,initLen + trainLen + initLen : initLen + trainLen + initLen + testLen].reshape(1,testLen)
 
+# Calculate the NMSE
 predicted_target = np.dot(Wout,nodeTS)
 predicted_target = predicted_target > 0.5
-
-# Calculate the MSE through L2 norm
-mse_testing = (((Yt - predicted_target)**2).mean(axis=1))
-
-# Calculate the NMSE
-nmse_testing  = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))**2
-nrmse_testing = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
-# nmse_testing  =         np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size
-# nrmse_testing = np.sqrt(np.sum((Yt - predicted_target)**2 / np.var(Yt)) / Yt.size)
+mse   = np.sum(np.power(Yt - predicted_target,2)) / Yt.size
+nrmse = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
 
 print('--------------------------------------------------')
 print('Testing Errors')
-print(f'testing MSE     = {mse_testing[0]}')
-print(f'testing NMSE    = {nmse_testing}')
-# print(f'testing NRMSE    = {nrmse_testing}')
+print(f'testing mse: {mse}')
+print(f'testing nrmse: {nrmse}')
 
 
 # DFR: An Energy-efficient Analog Delay Feedback Reservoir Computing System
