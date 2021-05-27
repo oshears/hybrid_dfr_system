798dbe482a62afca0b800a0e90fe1be7f98c936c
Osaze Shears <oshears@vt.edu>
2021-05-27 07:56:25 -0400

    got working complete narma10 dataset testing

--- linux/code_examples/dfr_fpga/dfr_hw_fpga_narma10_complete.py
+++ linux/code_examples/dfr_fpga/dfr_hw_fpga_narma10_complete.py
@@ -2,6 +2,7 @@ import os
 import mmap
 import time
 import numpy as np
+import math
 
 def int2bytes(data):
     return bytes([data & 0xFF, (data >> 8) & 0xFF, (data >> 16) & 0xFF, (data >> 24) & 0xFF])
@@ -43,18 +44,19 @@ NUM_STEPS_PER_SAMPLE = 100
 
 NUM_VIRTUAL_NODES = 100
 NUM_STEPS_PER_SAMPLE = NUM_VIRTUAL_NODES
-MAX_INPUT_SAMPLES_STEPS = 2 ** 16
+MAX_INPUT_SAMPLES_STEPS = int(2 ** 16 / NUM_STEPS_PER_SAMPLE) * NUM_STEPS_PER_SAMPLE
 MAX_INPUT_SAMPLES = int(MAX_INPUT_SAMPLES_STEPS / NUM_STEPS_PER_SAMPLE)
 
 # NUM_INIT_SAMPLES + NUM_TEST_SAMPLES must be less than MAX_INPUT_SAMPLES - 1 to prevent internal sample_cntr from overflowing  
-NUM_INIT_SAMPLES = 1
-NUM_TEST_SAMPLES = MAX_INPUT_SAMPLES - NUM_INIT_SAMPLES - 1
+NUM_INIT_SAMPLES = 100
+NUM_TEST_SAMPLES = MAX_INPUT_SAMPLES - NUM_INIT_SAMPLES
+NUM_TEST_SAMPLES_STEPS = NUM_TEST_SAMPLES * 100
 
-TOTAL_STEPS = 400100
-TOTAL_SAMPLES = 4001
+TOTAL_STEPS = 410000
+TOTAL_SAMPLES = 4100
 TOTAL_TEST_SAMPLES = TOTAL_SAMPLES - NUM_INIT_SAMPLES
 
-NUM_TEST_INPUT_BATCHES = int(TOTAL_TEST_SAMPLES / MAX_INPUT_SAMPLES)
+NUM_TEST_INPUT_BATCHES = math.ceil(TOTAL_TEST_SAMPLES / NUM_TEST_SAMPLES)
 
 # Configure Widths
 regs[NUM_INIT_SAMPLES_REG_ADDR : NUM_INIT_SAMPLES_REG_ADDR + 4] = int2bytes(NUM_INIT_SAMPLES)
@@ -65,13 +67,16 @@ regs[NUM_INIT_STEPS_REG_ADDR : NUM_INIT_STEPS_REG_ADDR + 4] = int2bytes(NUM_INIT
 regs[NUM_TRAIN_STEPS_REG_ADDR : NUM_TRAIN_STEPS_REG_ADDR + 4] = int2bytes(0)
 regs[NUM_TEST_STEPS_REG_ADDR : NUM_TEST_STEPS_REG_ADDR + 4] = int2bytes(NUM_TEST_SAMPLES * NUM_STEPS_PER_SAMPLE)
 
+print(f"Samples: {NUM_TEST_SAMPLES}, Steps: {NUM_TEST_SAMPLES * NUM_STEPS_PER_SAMPLE}")
+
 regs[NUM_STEPS_PER_SAMPLE_REG_ADDR : NUM_STEPS_PER_SAMPLE_REG_ADDR + 4] = int2bytes(NUM_STEPS_PER_SAMPLE)
 
 # Used to persistently keep track of inputs
 input_cntr = 0
+input_addr = 0
 
 # Keep track of DFR predictions
-predicted_target = np.ndarray(shape=(1,NUM_TEST_SAMPLES*NUM_TEST_INPUT_BATCHES))
+predicted_target = np.ndarray(shape=(1,TOTAL_TEST_SAMPLES))
 dfr_output_cntr = 0
 
 # Configure Input Samples
@@ -82,13 +87,18 @@ fh = open("dfr_sw_int_narma10_inputs.txt","r")
 input_file_lines = fh.readlines()
 fh.close()
 
-for i in range((NUM_TEST_SAMPLES + NUM_INIT_SAMPLES + 1) * NUM_STEPS_PER_SAMPLE):
-    sample_val = int(input_file_lines[i].strip())
-    regs[DFR_INPUT_MEM_ADDR_OFFSET + i*4 : DFR_INPUT_MEM_ADDR_OFFSET + i*4 + 4] = int2bytes(sample_val)
-    
-    if i < (NUM_TEST_SAMPLES + NUM_INIT_SAMPLES) * NUM_STEPS_PER_SAMPLE:
-        input_cntr += 1
+# Write Init Samples
+for i in range(NUM_INIT_SAMPLES):
+    sample_val = int(input_file_lines[input_addr].strip())
+    regs[DFR_INPUT_MEM_ADDR_OFFSET + input_cntr*4 : DFR_INPUT_MEM_ADDR_OFFSET + input_cntr*4 + 4] = int2bytes(sample_val)
+    input_addr += 1
+    input_cntr += 1
 
+
+# Write Test Samples
+for i in range(NUM_TEST_SAMPLES * NUM_STEPS_PER_SAMPLE):
+    sample_val = int(input_file_lines[input_addr].strip())
+    regs[DFR_INPUT_MEM_ADDR_OFFSET + input_cntr*4 : DFR_INPUT_MEM_ADDR_OFFSET + input_cntr*4 + 4] = int2bytes(sample_val)
 
 # Configure Weights
 print("Configuring Weight Memory")
@@ -120,25 +130,31 @@ for i in range(NUM_TEST_SAMPLES):
     dfr_output_cntr += 1
 
 
-# Reconfigure weights
+# Reconfigure sample counts
 regs[NUM_INIT_SAMPLES_REG_ADDR : NUM_INIT_SAMPLES_REG_ADDR + 4] = int2bytes(0)
 regs[NUM_INIT_STEPS_REG_ADDR : NUM_INIT_STEPS_REG_ADDR + 4] = int2bytes(0)
 
-for batch in range(1,NUM_TEST_INPUT_BATCHES):
+for batch in range(1,NUM_TEST_INPUT_BATCHES+1):
     print(f"Batch: {batch + 1} / {NUM_TEST_INPUT_BATCHES}")
 
     # Update inputs
-    INPUT_START = input_cntr
     input_pos = 0
-    for i in range(INPUT_START, INPUT_START + (NUM_TEST_SAMPLES + NUM_INIT_SAMPLES + 1) * NUM_STEPS_PER_SAMPLE):
-        sample_val = int(input_file_lines[i].strip())
+    for i in range(NUM_TEST_SAMPLES_STEPS):
+        if input_addr > TOTAL_STEPS - 1:
+            print("Last Input Step Reached")
+            break
+
+        sample_val = int(input_file_lines[input_addr].strip())
         regs[DFR_INPUT_MEM_ADDR_OFFSET + input_pos*4 : DFR_INPUT_MEM_ADDR_OFFSET + input_pos*4 + 4] = int2bytes(sample_val)
         
-        if i < (NUM_TEST_SAMPLES + NUM_INIT_SAMPLES) * NUM_STEPS_PER_SAMPLE:
-            input_cntr += 1
-
+        input_addr += 1
         input_pos += 1
 
+    BATCH_TEST_SAMPLES_STEPS = input_pos
+    BATCH_TEST_SAMPLES = int(input_pos / 100)
+    print(f"Number of Test Input Steps: {BATCH_TEST_SAMPLES_STEPS}, Samples: {BATCH_TEST_SAMPLES}, Addr: {input_addr}")
+    regs[NUM_TEST_SAMPLES_REG_ADDR : NUM_TEST_SAMPLES_REG_ADDR + 4] = int2bytes(BATCH_TEST_SAMPLES)
+    regs[NUM_TEST_STEPS_REG_ADDR : NUM_TEST_STEPS_REG_ADDR + 4] = int2bytes(BATCH_TEST_SAMPLES_STEPS)
 
     node_i = NUM_VIRTUAL_NODES - 1
     # use the node states from the last set of input samples
@@ -168,27 +184,34 @@ for batch in range(1,NUM_TEST_INPUT_BATCHES):
         continue
 
     # Read Outputs
+    output_val = 0
     print("Reading DFR Output Memory")
-    for i in range(NUM_TEST_SAMPLES):
+    for i in range(BATCH_TEST_SAMPLES):
+        if dfr_output_cntr > TOTAL_TEST_SAMPLES - 1:
+            print(f"Last Output: {output_val}")
+            print("test samples exceeded")
+            break
         output_val = bytes2int(regs[DFR_OUTPUT_MEM_ADDR_OFFSET + i*4 : DFR_OUTPUT_MEM_ADDR_OFFSET + i*4 + 4])
         predicted_target[0,dfr_output_cntr] = output_val
         dfr_output_cntr += 1
+        if i == NUM_TEST_SAMPLES - 1:
+            print(f"Last Output: {output_val}")
 
-    # Load Expected Data
-    Yt = np.ndarray(shape=(1,NUM_TEST_SAMPLES*NUM_TEST_INPUT_BATCHES))
-    expected_output_cntr = 0
-    fh = open("dfr_sw_int_narma10_expected_dfr_outputs.txt","r")
-    file_lines = fh.readlines()
-    fh.close()
-    for file_line in file_lines:
-        if expected_output_cntr < NUM_TEST_SAMPLES*NUM_TEST_INPUT_BATCHES:
-            Yt[0,expected_output_cntr] = float(file_line.strip())
-        expected_output_cntr += 1
+# Load Expected Data
+Yt = np.ndarray(shape=(1,TOTAL_TEST_SAMPLES))
+expected_output_cntr = 0
+fh = open("dfr_sw_int_narma10_expected_dfr_outputs.txt","r")
+output_file_lines = fh.readlines()
+fh.close()
+for expected_output_line in output_file_lines:
+    if expected_output_cntr < TOTAL_TEST_SAMPLES:
+        Yt[0,expected_output_cntr] = float(expected_output_line.strip())
+    expected_output_cntr += 1
 
 # Calculate the MSE through L2 norm
 mse   = np.sum(np.power(Yt - predicted_target,2)) / Yt.size
 nrmse = (np.linalg.norm(Yt - predicted_target) / np.linalg.norm(Yt))
-
+print(Yt.size)
 print('--------------------------------------------------')
 print('Testing Errors')
 print(f'testing mse: {mse}')
